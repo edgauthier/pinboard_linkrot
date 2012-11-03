83914afdb7cdf4c2ff0ba7e549646dbb397746a7
Ed Gauthier <ed@egauthier.net>
2012-11-03 00:06:41 -0400

    Initial version of Pinboard linkrot tool

---
+++ README.md
@@ -0,0 +1,11 @@
+Overview
+========
+
+Processes a json formatted Pinboard export and tests each link to determine if
+it's still reachable. A list of unreachable links is reported upon completion.
+
+Usage
+=====
+
+    Usage: pinboard_linkrot.py <bookmarks.json>
+
---
+++ pinboard_linkrot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import requests
+import json
+import sys
+from requests.exceptions import SSLError, InvalidSchema, ConnectionError
+
+def get_link_status_code(link):
+	headers = {'User-agent':'Mozilla/5.0'}
+	try:
+		r = requests.get(link, headers = headers)
+		return r.status_code
+	except (SSLError, InvalidSchema, ConnectionError):
+		return 409
+
+def is_valid_link(status_code):
+	if status_code == 200:
+		return True
+	else:
+		return False 
+	
+def process_links(links):
+    bad_links = 0
+    try:
+        for link in links:
+            status_code = get_link_status_code(link['href'])
+            if not is_valid_link(status_code):
+                print 'Invalid link (%s): %s [%s]' % (status_code, link['description'], link['href'])
+                bad_links += 1
+    except KeyboardInterrupt:
+        pass
+	
+	linkrot = int(bad_links/len(links)*100)
+	print '\n%s%% linkrot\n' % linkrot
+		
+def process_bookmarks_file(filename):
+	with open(filename) as f:
+		bookmarks = json.load(f)
+		process_links(bookmarks)
+		
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'Usage: pinboard_linkrot.py <bookmarks.json>'
+        exit(1)
+    process_bookmarks_file(sys.argv[1])
